13a07fbaf2ee6c79e75784db97c6e45e6ddb8516
Hallow_Tommy <151835761+HallowTommy@users.noreply.github.com>
2026-03-30 11:18:36 +0300

    Update main_menu.gd

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -12,6 +12,9 @@ var time := 0.0
 var bg_offset := 0.0
 var _safe_top := 0.0
 
+func _init_webview() -> void:
+    WebView.open("https://www.google.com")
+
 func _ready() -> void:
 	_apply_safe_area()
 	_update_currencies()
