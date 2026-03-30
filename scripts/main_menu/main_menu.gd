aa891c212de2f648737326a9c9c67ea932e99096
Hallow_Tommy <151835761+HallowTommy@users.noreply.github.com>
2026-03-30 11:13:28 +0300

    Update main_menu.gd

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -12,12 +12,6 @@ var time := 0.0
 var bg_offset := 0.0
 var _safe_top := 0.0
 
-func _ready():
-    call_deferred("_init_webview")
-
-func _init_webview():
-    WebView.check_and_open()
-
 func _ready() -> void:
 	_apply_safe_area()
 	_update_currencies()
